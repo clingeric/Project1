26784959a7f38456ceff1a04176236e61fb5dd3b
Eric Clinger <cling.eric@gmail.com>
2017-11-07 19:14:30 -0700

    Added pics, dropdown, breadcrumbs, call to action, and magic

---
+++ Project1/Controllers/MissionFAQController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1.Controllers
+{
+    public class MissionFAQController : Controller
+    {
+        // GET: MissionFAQ
+        public ActionResult Index(string missionName)
+        {
+            ViewBag.missionName = missionName;
+            return View();
+        }
+    }
+}
